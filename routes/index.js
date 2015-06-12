f4972cec1a7dd21b6fee35bea0668d01c1deace0
unknown <LAB-USR-I-PT355-602@UTP054408.grupoutp.net>
2015-06-11 22:16:41 -0500

    your message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
-var router = express.Router();
+var router 	= express.Router();
+var mysql      = require('mysql');
+var cnx = mysql.createConnection({
+			  host     : 'services-tec.com',
+			  user     : 'root',
+			  password : 'S0p0rt3',
+			  database : 'apirest'
+			});
+cnx.connect();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Apirest' });
+	cnx.query("Select * From usuarios",function(err,rows){
+		res.send(rows);
+	})
+	cnx.end();
+});
+
+router.get('/insert', function(req, res, next) {
+	var cod = req.query.cod;
+	console.log(cod);
+	cnx.query("Select * From usuarios where Codigo="+cod,function(err,rows){
+		res.send(rows);
+	})
+	cnx.end();
 });
 
 module.exports = router;
